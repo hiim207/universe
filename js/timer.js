2fcac95993feaeb0b5c381c5ec7abb65d4647fe7
hiim207 <20jin_lee@naver.com>
2023-11-23 11:04:47 +0900

    1123 오전 명언, 배경 추가

--- js/timer.js
+++ js/timer.js
@@ -5,7 +5,10 @@ const clock = document.getElementById("clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerText = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+    const hour = String(date.getHours()).padStart(2,"0");
+    const minute = String(date.getMinutes()).padStart(2,"0");
+    const second = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hour} : ${minute} : ${second}`;
 }
 
 getClock();
